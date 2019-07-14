f42c0a216365a856866a1a0d07fdbc330cfd61c2
Maggie <maggiejgray@gmail.com>
2019-07-13 19:12:12 -0500

    general frontend setup, webpack building

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      something: null
+    }
+  }
+
+  render() {
+    return (
+      <h1>hi maggie</h1>
+    )
+  }
+};
+
+export default App;
+
