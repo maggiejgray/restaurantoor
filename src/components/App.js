d1e0bd886ef0202f76e25fdc54eb4e2dd781d9bd
Maggie <maggiejgray@gmail.com>
2019-09-28 14:49:14 -0500

    add methods to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Roulette from './ReactWheel';
+import CheckList from './CheckList';
 import axios from 'axios';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
     this.state = {
       currentPage: 'landing',
       location: '',
+      coordinates: '',
       keyword: '',
       list: [],
       checked: [],
@@ -16,6 +18,10 @@ class App extends React.Component {
     }
 
     this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleKeywordChange = this.handleKeywordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.selectItem = this.selectItem.bind(this);
+    this.handleWheelDecision = this.handleWheelDecision.bind(this);
   }
 
   handleLocationChange(e) {
@@ -24,22 +30,63 @@ class App extends React.Component {
     });
   }
 
-  
   handleKeywordChange(e) {
     this.setState({
       location: e.target.value
     })
   }
-  
-  // on submit
-  handleSubmit() {
-    // axios get to /api/restaurants
 
-    // on response map results to create an array of name, id and setState of list
+  getLocation() {
+    return axios.get(`/location?search=${this.state.location}`)
+    .then((response) => {
+      let lat = String(response.data.results[0].geometry.location.lat);
+      let lng = String(response.data.results[0].geometry.location.lng);
+      this.setState({
+        coordinates: `${lat},${lng}`
+      })
+    })
+    .catch((error) => {
+      console.log('error getting location:', error);
+    })
   }
 
-  selectItem() {
-    // on click of checkboxes, add tuple to selected with name,id
+  getRestaurants() {
+    axios.get(`/restaurants?location=${this.state.coordinates}&keyword=${this.state.keyword}`)
+    .then((response) => {
+      this.setState({
+        list: response.data
+      })
+    })
+    .catch((error) => {
+      console.log('error getting restaurant list:', error);
+    });
+  }
+
+  // on submit
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    if (!this.state.coordinates) {
+      this.setState({
+        currentPage: 'wheel'
+      })
+  
+      this.getLocation()
+      .then(() => {
+        this.getRestaurants();
+      })
+      .catch((err) => {
+        console.log('error getting initial list of restaurants:', err);
+      })
+    } else {
+      this.getRestaurants();
+    }
+  }
+
+  selectItem(e) {
+    this.setState({
+      checked: this.state.checked.concat([e.target.name])
+    })
   }
   
   handleWheelDecision() {
@@ -51,23 +98,28 @@ class App extends React.Component {
   render() {
     return (
       <div id='container'>
+        <h1>restaurantor</h1>
+
+        {this.state.currentPage === 'landing' ?
         <div id='landingPage'>
-          <h1>restaurantor</h1>
           <form id='locationSearch'>
             <label>Enter location:</label><br></br>
-            <input type='text' id='location' onChange={this.handleLocationChange}></input>
-            <input type='submit' value='Search'></input>
+            <input type='text' id='location' onChange={this.handleLocationChange.bind(this)}></input>
+            <input type='submit' value='Search' onClick={(e) => (this.handleSubmit(e))}></input>
           </form>
         </div>
-
+        :
         <div id='wheelPage'>
           <form id='restaurantSearch'>
               <label>Add custom restaurants to wheel:</label><br></br>
-              <input type='text' id='restaurant' onChange={this.handleKeywordChange}></input>
+              <input type='text' id='keyword' onChange={this.handleKeywordChange}></input>
               <input type='submit' value='Search'></input>
           </form>
+          <CheckList restaurants={this.state.list} selectItem={this.selectItem}/>
+          {this.state.checked.length ? <Roulette options={this.state.checked} /> : undefined}
           <Roulette />
         </div>
+        }
 
         {/* <div id='decisionPage'>
           <h2>restaurant name here</h2>
