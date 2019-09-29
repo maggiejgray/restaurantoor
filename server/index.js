471b38675d0b7eae46b63917b3df9208be155c6c
Maggie <maggiejgray@gmail.com>
2019-09-29 16:51:59 -0500

    add catch all route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/../dist'));
 
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../dist/index.html'));
+});
+
 app.listen(port, () => { 
   console.log(`Listening on port ${port}`)
 });
