a92d172a192163cbc0c3cf88193f7555f437b6db
Maggie <maggiejgray@gmail.com>
2019-11-08 12:27:56 -0600

    starting over with roulette wheel

--- src/components/App.js
+++ src/components/App.js
@@ -106,42 +106,6 @@ class App extends React.Component {
   makeDecision(e) {
     e.preventDefault();
 
-    // declare variable to decide length of animation (random between 7 and 15)
-    // let animationTime = getRandomInt(5, 15);
-
-    // function getRandomInt(min, max) {
-    //   min = Math.ceil(min);
-    //   max = Math.floor(max);
-    //   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    // }
-
-    let increment = 200;
-    
-    // FIX THIS TERRIBLE TIME COMPLEXITY WHEN TIME ALLOWS!!!
-    while (200 <= increment <= 2000) {
-      for (let i = 0; i < this.state.selected.length; i++) {
-
-        // for (let j = 0; j !== i && j < this.state.selected.length; j++) {
-        //   console.log('!!!!!!!!!', document.getElementsByClassName('stackText'));
-        //   document.getElementsByClassName('stackText').style.display = none;
-        // }
-
-        console.log('!!!', document.getElementById(this.state.selected[i]));
-        document.getElementById(this.state.selected[i]).style.display = 'inline';
-        
-        setTimeout(() => {}, increment);
-      }
-
-      increment = increment + 200;
-    }
-
-    // get currently displayed element and pop up modal
-    // iterate through selected at slowing speeds for x amount of seconds
-
-    //   change style of <p> of all of elements other than current to display: none
-    
-
-    // on decision, use key/placeID to getRestaurantDetails()
   }
 
   getRestaurantDetails() {
